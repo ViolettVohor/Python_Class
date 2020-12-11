381e9f8216a8b3dcc755d3b498673b06735b87e2
MatthewsTomts <71537027+MatthewsTomts@users.noreply.github.com>
2020-12-10 21:49:41 -0300

    Update Aula11A.py

---
+++ PythonTest/Aula11A.py
@@ -0,0 +1,14 @@
+print('\033[1;32mOlá, Mundo!\033[m')
+
+a = 3
+b = 5
+print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
+
+cores = {'limpa': '\033[m',
+         'verde': '\033[32m',
+         'amarelo': '\033[34m',
+         'pretoebranco': '\033[7;30m',
+         'negrito': '\033[1m'}
+
+nome = 'Oliver'
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['verde'], nome, cores['limpa']))
