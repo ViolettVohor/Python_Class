80a69aaf42e01d5c2163d98171d31d22aa5c9b23
MatthewsTomts <71537027+MatthewsTomts@users.noreply.github.com>
2021-03-23 22:54:49 -0300

    Comentando #1.1

--- PythonExercicios/ex001.py
+++ PythonExercicios/ex001.py
@@ -1,4 +1,5 @@
-msg = '\033[32;1mOlá, Mundo!' #cria a variável 'msg'
-                              # com o valor "Olá, mundo" do tipo string
-                              #na cor da fonte verde (32) e em negrito (1)
-print(msg)  #escreva o valor da variável msg na tela
+msg = '\033[32;1mOlá, Mundo!'
+# Cria a variável 'msg' com o valor "Olá, mundo" do
+# tipo string na cor da fonte verde (32) e em negrito (1)
+print(msg)
+# Escreva o valor da variável msg na tela
--- PythonExercicios/ex002.py
+++ PythonExercicios/ex002.py
@@ -1,6 +1,6 @@
-nome = input('\033[30;1mDigite seu nome: ') # a variável nome recebe o valor digitado pelo usuário
-                                            # em resposta ao pedido escrito dentro de input "Digite seu nome"
-                                            # 0 Branco
-print(f'É um prazer te conhecer, \033[36;1m{nome}!')    # escreva um texto na tela
-                                                        # com a nova sintaxe 'f' para referênciação da variável nome
-                                                        # 6 turquesa
+nome = input('\033[30;1mDigite seu nome: ')
+# A variável nome recebe o valor digitado pelo usuário em resposta ao pedido escrito dentro de input "Digite seu nome"
+# 0 Branco
+print(f'É um prazer te conhecer, \033[36;1m{nome}!')
+# Escreve um texto na tela com a nova sintaxe 'f' para referenciação da variável nome
+# 6 turquesa
