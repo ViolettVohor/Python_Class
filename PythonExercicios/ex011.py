67321bd8cd5db8d0ff1b68ec40ff0bdd0db9c573
MatthewsTomts <71537027+MatthewsTomts@users.noreply.github.com>
2021-04-09 20:42:17 -0300

    Update ex011.py

--- PythonExercicios/ex011.py
+++ PythonExercicios/ex011.py
@@ -1,3 +1,4 @@
 lar = float(input('Qual a largura da parede, em metros? '))
 alt = float(input('Qual a altura da parede, em metros? '))
+# Pede ao usuário que digite as medida da parede
 print(f'Sua parede tem \033[31;1m{lar*alt:.1f}m²\033[m e você precisa de \033[36;1m{lar*alt/2:.1f}\033[m Litros de tinta')
